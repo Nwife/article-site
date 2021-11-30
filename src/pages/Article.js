79d64e952e6549596d453708a4e394417fd2f050
Nwife Amaeze <ifeeamaeze@gmail.com>
2021-11-30 07:11:47 +0100

    fetch the data using the useFetch hook and params id

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,7 +1,21 @@
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
+
 export default function Article() {
+    const { id } = useParams(); //the id is a property on the params object |we destructured it|
+    const url = 'http://localhost:8000/articles/' + id;
+    const {data: article, isPending, error} = useFetch(url);
     return (
         <div>
-            Article page
+            {isPending && <div>loading...</div>}
+            {error && <div>{error}</div>}
+            {article && (
+                <div>
+                    <h2>{article.title}</h2>
+                    <p>By {article.author}</p>
+                    <p>{article.body}</p>
+                </div>
+            )}
         </div>
     )
 }
